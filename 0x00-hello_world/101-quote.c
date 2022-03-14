9ba86797253299ba85c6e83c52204f4c602b3f17
root <ajimobikareemah@gmail.com>
2022-03-14 07:10:05 -0700

    fixed abug

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,9 @@
 #include <unistd.h>
 
 /*
- * main - Entry point
+ * main - prints exactly and that piece of art is useful
+ * -Dora Korper, 2015-10-19,
+ *  followed by a new line, to the standard error.
  * Return: Always 0 (Success)
  */
 int main(void)

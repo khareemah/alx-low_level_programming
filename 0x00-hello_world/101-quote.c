1b18bf188c1216b0515d62374d31fd55d436fd19
root <ajimobikareemah@gmail.com>
2022-03-14 07:11:33 -0700

    fixed abug

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,9 +2,7 @@
 #include <unistd.h>
 
 /*
- * main - prints exactly and that piece of art is useful
- * -Dora Korper, 2015-10-19,
- *  followed by a new line, to the standard error.
+ * main - prints exactly and that piece of art is useful -Dora Korper, 2015-10-19, followed by a new line, to the standard error.
  * Return: Always 0 (Success)
  */
 int main(void)

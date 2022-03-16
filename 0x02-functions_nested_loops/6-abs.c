770811a3b7e7c967b5a7ec4eebbda1972a78f41d
root <ajimobikareemah@gmail.com>
2022-03-16 05:16:44 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _abs - returns absolute value
+* @c: value to be checked
+* Return: Always 0;
+*/
+int _abs(int c)
+{
+	if (c < 0)
+{
+	return (c * -1);
+}
+	else
+{
+	return (c);
+}
+}

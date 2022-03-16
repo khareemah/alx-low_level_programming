fc1768f84a01f6ec3c7f7a04f232f4ad80f8c49e
root <ajimobikareemah@gmail.com>
2022-03-16 03:15:17 -0700

    alphabeths

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
- * print_alphabet - loop from a - z
- * Return: Always 0.
+ * print_alphabet - check the code
+ * Return: List of alphabeth from a - z
  */
+
 void print_alphabet(void)
 {
 	char ch;
 
-	for (ch = 'a' ; ch <= 'z'; ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-		_putchar('a');
-		return;
+		_putchar(ch);
 	}
+	_putchar('\n');
 }

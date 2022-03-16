29aa8c9adc207dcb066c7793119cd4605f126f15
root <ajimobikareemah@gmail.com>
2022-03-16 04:44:48 -0700

    alpha and numeric

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isalpha - Return 1 if c is a letter. lowercase or uppercase
+ *
+ *@c: The int to print
+ * Return: Always 0.
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 48 && c <= 122)
+{
+	return (1);
+}
+	else
+{
+	return (0);
+}
+}

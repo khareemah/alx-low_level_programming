66fa76c99cefcd4fee1a1492042838edb5e439ad
root <ajimobikareemah@gmail.com>
2022-03-16 07:09:46 -0700

    print every minute of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,19 +1,20 @@
 #include "main.h"
-/*
-* jack_bauer - prints date
-* @i: first variable
-* @j: second variable
+
+/**
+* jack_bauer - prints minutes of the day
+* Return: Always 0.
 */
 
 void jack_bauer(void)
 {
 	int i;
 	int j;
-	for(i = 0; i <= 23; i++)
+
+	for (i = 0; i <= 23; i++)
 	{
-	for(j = 0; j < 60; j++)
+	for (j = 0; j < 60; j++)
 {
-	_putchar(i/10 + '0');
+	_putchar(i / 10 + '0');
 	_putchar (i % 10 + '0');
 	_putchar (':');
 	_putchar (j / 10 + '0');

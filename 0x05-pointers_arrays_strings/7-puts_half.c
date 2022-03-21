2a57637034c1c43dde3309baf95ee7da59095945
root <ajimobikareemah@gmail.com>
2022-03-21 05:36:10 -0700

    print second half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* puts_half - prints second half of a string
+* @str: string parameter
+* Return: void
+*/
+
+void puts_half (char *str)
+{
+	int i;
+	int j;
+	for (i = 0; str[i] != '\0'; i++)
+{
+	i++;
+}
+	for (j = i/2; str[j] != '\0'; j++)
+{
+	_putchar(str[j]);
+}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,5 +7,5 @@ void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void puts2(char *str);
-
+void puts_half(char *str);
 #endif

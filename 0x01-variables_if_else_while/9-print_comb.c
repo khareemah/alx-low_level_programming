6669871813d784f0236f171b4212c5d5c4b6b6a6
root <ajimobikareemah@gmail.com>
2022-03-22 08:06:23 -0700

    prints 0-9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+* main - entry point
+* Description: Posible combination of an integer
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+{
+	putchar(i + '0');
+	if (i != 9)
+{
+	putchar(',');
+	putchar(' ');
+	
+}
+
+}
+	putchar('\n');
+	return (0);
+}

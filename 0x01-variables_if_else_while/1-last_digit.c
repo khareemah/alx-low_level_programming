d1d1ab11b15144c3f9565b86f3fb45e60f769ed7
root <ajimobikareemah@gmail.com>
2022-03-22 03:56:03 -0700

    check last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,18 +13,19 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	lastDigit = n % 10;
+
 	if (lastDigit > 5)
-	{
-		printf("Last digit of %i is %i and is greater than 5", n, lastDigit);
-	}
-	else if (lastDigit < 0 && lastDigit != 0)
-	{
-		printf("Last digit of %i is %i and is less than 6 and not 0", n, lastDigit);
-	}
-	else if (lastDigit < 6)
-	{
-		printf("lastDigit of %i is %i and is 0", n, lastDigit);
-	}
+{
+	printf("Last digit of %i is %i and is greater than 5\n", n, lastDigit);
+}
+	else if (lastDigit < 6 && lastDigit != 0)
+{
+	printf("Last digit of %i is %i and is less than 6 and not 0", n, lastDigit);
+}
+	else if (lastDigit == 0)
+{
+	printf("last digit of %i is %i and is \n", n, lastDigit);
+}
 	/* your code goes there */
 	return (0);
 }	
---
+++ 0x01-variables_if_else_while/4
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - assigns a randon number to int n everytime
+ * it executes, and print it
+ * Return: ALways 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+
+	if (n  > 0)
+	{
+		printf("%i is positive\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%i is negative\n", n);
+	}
+	else
+	{
+		printf("%i is zero\n", n);
+	}
+	return (0);
+}

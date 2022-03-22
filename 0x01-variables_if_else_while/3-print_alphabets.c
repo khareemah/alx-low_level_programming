f0cbc95a657dfb79c72fff5ca8d9c7163311ac52
root <ajimobikareemah@gmail.com>
2022-03-22 04:41:54 -0700

    print upper and lowercase alpha

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
+
+/**
+* main - main function
+* Description: prints uppercase and lowercase alpha
+* Return: 0
+*/
+
 int main(void)
 {
-	char ch;
-	for(ch = "A"; ch <= "Z"; ch++)
-	{
-	  putchar(ch);
-	}
+	char ch[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; ch[i] != '\0'; i++)
+{
+	putchar(ch[i]);
+}	
+	putchar('\n');
+	return (0);
 }

6936b2a66c52225facb33511a556a936c858847b
root <ajimobikareemah@gmail.com>
2022-03-22 04:05:26 -0700

    print character in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - main block
+* Description: Prints alphabets in lower case
+* Return: 0
+*/
+
+int main(void)
+{
+	char alpha[] = "abcdefghijklmopqrstuvwxyz";
+	int i = 0;
+
+	while (alpha[i] != '\0')
+{
+	putchar(alpha[i]);
+	i++;
+}
+	putchar('\n');
+	return (0);
+}

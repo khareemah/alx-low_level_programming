17875fb4fddcc3da3e647ad10682f9ada773f282
root <ajimobikareemah@gmail.com>
2022-03-24 05:34:56 -0700

    copy teo strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,4 +1,22 @@
 #include "main.h"
 /**
 * _strncpy - copies a string
-* * 
+* @des: destination string
+* @src: source string
+* @n numbers of characters
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; src[i] != 0 && i < n; i++)
+{
+	dest[i] = src[i];
+}
+	for ( ; i < n; i++)
+{
+	dest[i] = '\0';
+}
+	return (dest);
+}

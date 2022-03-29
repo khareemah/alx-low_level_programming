3cf6639b2f27be0ef299f7cb71c99d99cb7b9bf2
root <ajimobikareemah@gmail.com>
2022-03-29 08:27:32 -0700

    refactor code

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,19 +7,13 @@
 
 int print_last_digit(int c)
 {
-	int last_digit;
+	int last_digit = c % 10;
 
-	if (c > 0)
+	if (c < 0)
 {
-	last_digit = c % 10;
-	_putchar(last_digit + '0');
-	return (last_digit);
+	last_digit *= -1;
 }
-	else
-{
-	last_digit = (-1 * (c % 10));
 	_putchar('0' + last_digit);
 	return (last_digit);
 
-}
 }

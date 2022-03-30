f86aec87426a0cf60735b17232586c6688a05f6a
root <ajimobikareemah@gmail.com>
2022-03-30 05:23:45 -0700

    return square root of a number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * helper - helper function fr square root
+ * @n: number
+ * @guess: guess number starting from 1
+ * Return: Always integer.
+ */
+int helper(int n, int guess)
+{
+	if (guess * guess == n)
+{
+	return (guess);
+}
+	else
+{
+	guess++;
+	return (helper(n, guess));
+}
+	if (guess * guess > n)
+{
+	return (-1);
+}
+}
+
+
+
+/**
+ * _sqrt_recursion - return natural square root
+ * @n: number
+ * Return: Always integer.
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+{
+	return (0);
+}
+	return (helper(n, 1));
+}

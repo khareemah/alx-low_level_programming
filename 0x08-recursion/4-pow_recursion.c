54c1a7dde51eb6e12f90a380bb89863fa569ed19
root <ajimobikareemah@gmail.com>
2022-03-30 04:27:52 -0700

    return exponent of two numbers

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - return the value if x ** y
+ * @x: index
+ * @y: power
+ * Return: Always 0.
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y > 0)
+{
+	return (x * _pow_recursion(x, (y - 1)));
+}
+	if (y == 0)
+{
+	return (1);
+}
+	return (-1);
+}

500e02d23d653a9aa1ba0ea2cd367a3f46eb441e
root <ajimobikareemah@gmail.com>
2022-03-31 07:37:45 -0700

    refactor code

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,8 +1,7 @@
 #include "main.h"
 /**
-* print_triangle - prints a triangle
+* print_traingle - prints a triangle
 * @size: parameter
-* Return: Always 1
 */
 
 void print_triangle(int size)
@@ -15,15 +14,12 @@ void print_triangle(int size)
 {
 	int i;
 	int j;
-	int k;
 
-	for (i = 0; i < size; i++)
+	for (i = 1; i <= size; i++)
 {
-	for (k = 0; k < 1; k++)
+	for (j = 0; j < size; j++)
 {
-	for (j = size; j >= 0; j--)
-{
-	if (j > i + k)
+	if(i + j < size)
 {
 	_putchar(' ');
 }
@@ -32,8 +28,7 @@ void print_triangle(int size)
 	_putchar('#');
 }
 }
-}
-	_putchar('\n');
+	_putchar('\n');	
 }
 	_putchar('\n');
 }

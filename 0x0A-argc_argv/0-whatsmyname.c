b73f81bc35f09f3cc38b2ddea87dc228bcb993cd
root <ajimobikareemah@gmail.com>
2022-04-01 03:24:11 -0700

    print name of file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * @argc: list of command line arguments
+ * @argv: array of command line argument
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - 1]);
+	return (0);
+}

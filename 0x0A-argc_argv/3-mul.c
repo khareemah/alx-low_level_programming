c1c108bbeb2f1f084bf4edaa7526a4e02feee012
root <ajimobikareemah@gmail.com>
2022-04-01 04:21:36 -0700

    fix betty warnings

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,8 +3,8 @@
 
 /**
 * main - multiply two digits
-* argc: argument count
-* argv: argument array
+* @argc: argument count
+* @argv: argument array
 * Return: 0
 */
 
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 {
 	int i;
 	int product = 1;
-	
+
 	if (argc <= 2)
 {
 	printf("ERROR\n");
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - add two integers
+* @argc: argument count
+* @argv: argument array
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if(argc <= 1)
+{
+	printf("0\n");
+}
+	else
+{
+	for ( 
+}
+}

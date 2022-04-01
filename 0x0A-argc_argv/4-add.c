713964332de9fb764e519975708fb5f8702ad3d5
root <ajimobikareemah@gmail.com>
2022-04-01 05:28:56 -0700

    add natural numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,13 +11,26 @@
 int main(int argc, char *argv[])
 {
 	int i;
+	int j;
+	int sum = 0;
 
-	if(argc <= 1)
-{
+	if (argc <= 1)
+	{
 	printf("0\n");
-}
-	else
-{
-	for ( 
-}
+	return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
 }

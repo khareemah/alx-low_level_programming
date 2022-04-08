ce14256bcfa8288122c9a8bffd66751081528be3
root <ajimobikareemah@gmail.com>
2022-04-08 05:42:04 -0700

    add posibble combinations of two digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 48; i < 58; i++)
+{
+	for (j = (i + 1); j < 58; j++)
+	{
+		putchar(i);
+		putchar(j);
+		if ((i + j) < ('8' + '9'))
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+}
+	putchar('\n');
+	return (0);
+}

d1da54c67fd1c4bb6c484979accb974d3be1aa32
root <ajimobikareemah@gmail.com>
2022-04-11 04:49:07 -0700

    initialize a variable of type struct dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,11 +7,13 @@
  * @name: Second param
  * @owner: Third param
  * @age: Fourth param
+ * Return: void
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	d.name = name;
+	d.age = age;
 	d.age = age;
 	d.owner = owner;
 }

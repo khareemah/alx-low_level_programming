9be75a6a0d2c25080ec6481da9b93aaa162b013e
root <ajimobikareemah@gmail.com>
2022-04-11 05:05:57 -0700

    finally compiled

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "main.h"
+#include "dog.h"
 
 /**
  * init_dog - function to initialize variable of type strict dog
@@ -12,8 +12,8 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d.name = name;
-	d.age = age;
-	d.age = age;
-	d.owner = owner;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+
 }

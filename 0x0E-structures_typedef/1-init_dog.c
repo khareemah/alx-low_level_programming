6ce747c4fa57054d4787057cd4893b190769f502
root <ajimobikareemah@gmail.com>
2022-04-11 04:20:17 -0700

    dog struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * init_dog - function to initialize variable of type strict dog
+ * @d: First param
+ * @name: Second param
+ * @owner: Third param
+ * @age: Fourth param
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d.name = name;
+	d.age = age;
+	d.owner = owner;
+}
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * struct dog - structer datatype for dog
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};

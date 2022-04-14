3fb19aa49011dd3436c287c949b6697a2047cd9e
root <ajimobikareemah@gmail.com>
2022-04-14 05:27:46 -0700

    print numbers followed by a new line

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,8 +18,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	if (i == n - 1)
 		printf("%d", va_arg(ap, int));
-	else
+	else 
+	{
+	if (separator != NULL)
 		printf("%d%s", va_arg(ap, int), separator);
+	}
 }
 	printf("\n");
 	va_end(ap);

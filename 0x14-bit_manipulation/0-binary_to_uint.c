0fb983ee22f78225d48d60ea2cb63c32acb17f7b
root <ajimobikareemah@gmail.com>
2022-04-28 05:45:58 -0700

    prints the binary representation of a number

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,10 +1,10 @@
 #include "main.h"
-
 /**
-* binary_to_unit - converts binary nymber to an unsigned int
-* @b: string of binary number
-* Return: coverted number or zero or null
-*/
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: points to a string of 0 and 1 characters
+ * Return: the converted number or 0 if there is one or more characters in
+ * string @b that is not 0 or 1, or @b is NULL
+ */
 
 unsigned int binary_to_uint(const char *b)
 {

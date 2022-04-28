edd95e3271e00a9eb552761230a966ce1de9531a
root <ajimobikareemah@gmail.com>
2022-04-28 05:26:34 -0700

    convert binary to integer

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+* binary_to_unit - converts binary nymber to an unsigned int
+* @b: string of binary number
+* Return: coverted number or zero or null
+*/
+
+unsigned int binary_to_uint(const char *b)
+{
+	int i = 0;
+	unsigned int n = 0;
+	int power = 0;
+
+	if (*b == '\0')
+		return (0);
+
+	while (b[i + 1])
+		i++;
+
+	while (i >= 0)
+	{
+		if (b[i] == '0')
+		{
+			i--;
+			power++;
+		}
+		else if (b[i] == '1')
+		{
+			n += (1 << power);
+			i--;
+			power++;
+		}
+		else
+			return (0);
+	}
+	return (n);
+
+
+}

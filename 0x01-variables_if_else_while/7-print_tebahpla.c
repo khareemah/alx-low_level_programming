b95fe6cbb4bac59d0e4acc807a47dd21191e715a
Khareemah <ajimobikareemah@gmail.com>
2022-06-07 05:39:58 -0700

    print alphabets

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - print digits
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 'z'; i >= 'a'; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}

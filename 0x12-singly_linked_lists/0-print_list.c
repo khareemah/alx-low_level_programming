48b4cab4e748bf5435dec923c2bdd4c188aecb87
kareemah <ajimobikareemah@gmail.com>
2022-06-07 09:29:12 +0100

    what the hell

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
 
+size_t print_list(const list_t *h)
+{
+	int counter = 0;
+
+	if (h->str == NULL)
+	{
+		printf("[0] (nil)");
+	}
+	else
+	{
+		while (h != NULL)
+		{
+			printf("[%u] %s\n", h->len, h->str);
+			h = h->next;
+			counter++;
+		}
+	}
+	return (counter);
+}

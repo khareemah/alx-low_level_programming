cfd82bcd0cd83875fbf998c04e5232acf1e6d270
Khareemah <ajimobikareemah@gmail.com>
2022-06-09 08:56:25 -0700

    return node at a specific position

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -3,19 +3,22 @@
 /**
 * get_dnodeint_at_index - get node at an index
 * @head: head pointer
-* @iindex: position to get node
+* @index: position to get node
+* Return: node at index
 */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int count;
 	dlistint_t *temp;
 
+	if (head == NULL)
+		return (NULL);
 	count = 0;
 	temp = head;
 	while (temp != NULL)
 	{
 		if (count == index)
-			return (temp);
+			return temp;
 		temp = temp->next;
 		count++;
 	}

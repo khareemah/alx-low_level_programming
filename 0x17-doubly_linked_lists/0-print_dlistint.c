2fed3562eecebf0a1e4355299037dce9f860f7b2
Khareemah <ajimobikareemah@gmail.com>
2022-06-09 04:27:50 -0700

    remove white space

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -5,12 +5,12 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	int counter = 0;
 	const dlistint_t *temp = h;
-	
+
 	while (temp != NULL)
 	{
 		printf("%d\n", temp->n);
 		temp = temp->next;
 		counter++;
 	}
-	return counter;
+	return (counter);
 }

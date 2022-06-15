3f78a7f96b516efca6e18d75acf16586466224ca
Khareemah <ajimobikareemah@gmail.com>
2022-06-15 03:25:37 -0700

    delete repo

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+* main - print numbers
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0; i < 10; i++)
+	{
+	for (j = 1; j < 10; j++)
+	{
+	for (k = 2; k < 10; k++)
+	{
+		putchar(i + '0');
+		putchar(j + '0');
+		putchar(k + '0');
+		putchar(' ');
+		/*if (i != 7 && j != 8 && k != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}*/
+	}
+	}
+	}
+	return (0);
+}
--- 0x02-functions_nested_loops/8-main.c
+++ 0x02-functions_nested_loops/8-main.c
@@ -7,9 +7,6 @@
  */
 int main(void)
 {
-	char *str;
-
-	str = "0123456789";
-	puts2(str);
+	jack_bauer();
 	return (0);
 }

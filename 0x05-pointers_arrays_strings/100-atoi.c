f8905d58f3ccec3dac19062b9c464f1e444223bc
Khareemah <ajimobikareemah@gmail.com>
2022-06-23 08:07:12 -0700

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,22 +9,22 @@
 
 int _atoi(char *s)
 {
+    int sign = 1;
+    int ans = 0;
 
-	int sign = 1;
-	unsigned int num = 0;
+    int i;
 
-	do {
+    for (i = 0; s[i] != '\0'; i++)
+    {
+        if (s[i] == '-')
+            sign = sign * -1;
+        else if (s[i] >= '0' && s[i] <= '9')
+        {
+            ans = (ans * 10) + (s[i] - '0');
+        }
 
-		if (*s == '-')
-			sign *= -1;
-
-		else if (*s >= '0' && *s <= '9')
-			num = (num * 10) + (*s - '0');
-
-		else if (num > 0)
-			break;
-
-	} while (*s++);
-
-	return (num * sign);
+        else if(ans > 0)
+		break;
+    }
+    return (ans * sign);
 }

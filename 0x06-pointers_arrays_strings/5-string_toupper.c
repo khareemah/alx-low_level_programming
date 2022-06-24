bee34f9c8b3c69100f0fa90e6ec7abef84d934ed
Khareemah <ajimobikareemah@gmail.com>
2022-06-24 05:58:47 -0700

    convert uppercase to lowercase string

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,15 +7,12 @@
 
 char *string_toupper(char *str)
 {
-	int i = 0;
+	int i;
 
-	while (str[i] != '\0')
-{
-	if (str[i] >= 'a' && str[i] <= 'z')
-{
-	str[i] = str[i] - 32;
-}
-	i++;
-}
+	for (i = 0; str[i]!= '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+	}
 	return (str);
 }

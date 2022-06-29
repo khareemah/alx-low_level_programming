cc5c14d7b5b43b1368ff99c473680a2b93fd3433
Khareemah <ajimobikareemah@gmail.com>
2022-06-29 06:14:54 -0700

    concatenate two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,40 +14,41 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	char *s3;
-	unsigned int s1len = 0;
-	unsigned int s2len = 0;
-	unsigned int s3len;
-	unsigned int i = 0;
-	unsigned int j = 0;
+	int _s1len;
+	int _s2len;
+	int total_char;
+	int i;
+	int j;
+	char *new_str;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
+	_s1len = 0;
+	while(s1[_s1len])
+		_s1len++;
 
-	while (s1[s1len])
-		s1len++;
-	while (s2[s2len])
-		s2len++;
+	_s2len = 0;
+	while(s2[_s2len])
+		_s2len++;
 
-	s3len = s1len + s2len;
-
-	s3 = malloc(sizeof(char) * s3len + 1);
-	if (s3 == NULL)
+	total_char = _s1len + _s2len + 1;
+	new_str = malloc(sizeof(char) * total_char);
+	if (new_str == NULL)
 		return (NULL);
 
-	while (i < s1len)
+	i = 0;
+	while (i < _s1len)
 	{
-		s3[i] = s1[i];
+		new_str[i] = s1[i];
 		i++;
 	}
 
-	while (i <= s3len)
+	j = 0;
+	while ( j < _s2len)
 	{
-		s3[i] = s2[j];
+		new_str[i] = s2[j];
 		i++;
 		j++;
 	}
-	return (s3);
+	new_str[total_char] = '\0';
+
+	return (new_str);
 }

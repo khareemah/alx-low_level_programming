ba8ee3791f7c941d9fff7d9db382dbe5b71b392c
Khareemah <ajimobikareemah@gmail.com>
2022-07-04 03:40:06 -0700

    inherits from list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module that contains a class MyList that inherits a list"""
+
+
+class MyList(list):
+    """Classs that inherits a list"""
+
+    def print_sorted(self): 
+        print(sorted(self))
+    
